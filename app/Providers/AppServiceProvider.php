5d4ae467c032514c9bb204120b90fcb0eb05d240
Yumeidasari <yumeidasari@gmail.com>
2021-05-18 07:58:54 +0000

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        if(config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
+        
 		\Schema::defaultStringLength(191);
 		config(['app.locale' => 'id']);
 		\Carbon\Carbon::setLocale('id');
